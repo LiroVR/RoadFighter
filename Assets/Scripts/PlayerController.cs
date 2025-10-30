18f960ba6114e23384b81a878a32b000665eb6de
Zector1114 <zectorssector@gmail.com>
2025-10-30 17:58:16 -0400

    added player side movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    RigidBody2D rb;
+    public Vector2 sideForce = (0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<RigidBody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sideForce, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sideForce, ForceMode2D.Force);
+        }
     }
 }
