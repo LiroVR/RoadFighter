fa653af0176fc9e7711f592965713902e97c1b66
Zector1114 <zectorssector@gmail.com>
2025-10-30 17:44:45 -0400

    timer is in

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static GameManager instance;
+
+    public float timer = 0f;
+    public float endTime = 60f;
+    public bool timeIsUp = false;
+
+    void Awake()
     {
-        
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer == endTime)
+        {
+            timeIsUp = true;
+        }
     }
 }
